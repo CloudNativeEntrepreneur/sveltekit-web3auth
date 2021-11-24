7f70763ab046d57682a0d873e0cb309c03becbab
Patrick Lee Scott <pat@patscott.io>
2021-11-23 19:27:42 -0600

    fix: format

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,20 +1,20 @@
 import type { Locals } from "$lib/types";
 import type { RequestHandler } from "@sveltejs/kit";
 
-const clientSecret = import.meta.env.VITE_WEB3_AUTH_CLIENT_SECRET
-const issuer = import.meta.env.VITE_WEB3_AUTH_ISSUER
+const clientSecret = import.meta.env.VITE_WEB3_AUTH_CLIENT_SECRET;
+const issuer = import.meta.env.VITE_WEB3_AUTH_ISSUER;
 
 /**
  * @type {import('@sveltejs/kit').RequestHandler}
  */
 export const post: RequestHandler<Locals, FormData> = async (request) => {
-  const clientId = (request.body as any).clientId
-  const publicAddress = (request.body as any).publicAddress
-  const signature = (request.body as any).signature
-  const Authorization = `Basic ${btoa(`${clientId}:${clientSecret}`)}`
+  const clientId = (request.body as any).clientId;
+  const publicAddress = (request.body as any).publicAddress;
+  const signature = (request.body as any).signature;
+  const Authorization = `Basic ${btoa(`${clientId}:${clientSecret}`)}`;
 
-  console.log('POST /auth/login', { Authorization })
-  let auth
+  console.log("POST /auth/login", { Authorization });
+  let auth;
   const createAuthSessionFetch = await fetch(`${issuer}/api/auth`, {
     body: JSON.stringify({ publicAddress, signature }),
     headers: {
@@ -22,20 +22,20 @@ export const post: RequestHandler<Locals, FormData> = async (request) => {
       "Content-Type": "application/json",
     },
     method: "POST",
-  })
+  });
 
   if (createAuthSessionFetch.ok) {
-    auth = await createAuthSessionFetch.json()
+    auth = await createAuthSessionFetch.json();
   }
 
   const user = JSON.parse(atob(auth.idToken.split(".")[1]).toString());
-  delete user.aud
-  delete user.exp
-  delete user.iat
-  delete user.iss
-  delete user.sub
-  delete user.typ
-  
+  delete user.aud;
+  delete user.exp;
+  delete user.iat;
+  delete user.iss;
+  delete user.sub;
+  delete user.typ;
+
   const cookie = `userInfo=${JSON.stringify({
     userid: user.publicAddress,
     user,
