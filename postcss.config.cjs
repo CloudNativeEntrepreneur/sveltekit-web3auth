15f0235ce127d16e7675b054a50348af8644a801
Patrick Lee Scott <pat@patscott.io>
2021-12-09 13:26:15 -0600

    chore: update example to use tailwindcss@3

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,28 @@
+const postcssImport = require("postcss-import");
 const tailwindcss = require("tailwindcss");
+const nesting = require("tailwindcss/nesting");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
+const presetEnv = require("postcss-preset-env")({
+  stage: 1,
+  features: {
+    // tailwind plugin handles
+    "nesting-rules": false,
+  },
+});
 
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 
 const config = {
   plugins: [
-    //Some plugins, like postcss-nested, need to run before Tailwind,
-    tailwindcss(),
+    postcssImport,
+    nesting,
+    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+    tailwindcss,
     //But others, like autoprefixer, need to run after,
-    autoprefixer(),
+    autoprefixer,
+    presetEnv,
     !dev &&
       cssnano({
         preset: "default",
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,11 @@
 const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   mode: "jit",
-  purge: ["./src/**/*.{html,js,svelte,ts}"],
+
   theme: {
     extend: {},
   },
+
   plugins: [],
 };
 
